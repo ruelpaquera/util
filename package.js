64fb08d4ccdfa3471efe571fe50378f3a3968c0e
Morten Henriksen <mh@gi2.dk>
2014-12-20 11:06:22 +0100

    Bump to version 0.1.8

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:util",
-  version: "0.1.7",
+  version: "0.1.8",
   summary: "Adds utillity functions for ground db to use",
   git: "https://github.com/GroundMeteor/util.git"
 });
