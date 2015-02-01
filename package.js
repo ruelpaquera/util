eccf2ee58455271cf8523de9b40aa7bdefab29e4
Morten Henriksen <mh@gi2.dk>
2015-02-01 10:25:24 +0100

    Bump minimax

--- package.js
+++ package.js
@@ -22,7 +22,7 @@ Package.onUse(function (api) {
     'minimongo',
     'ejson',
     'reactive-var',
-    'ground:minimax@1.1.1'
+    'ground:minimax@1.1.2'
     ], ['client', 'server']);
 
   api.imply('ground:minimax', ['client', 'server']);
