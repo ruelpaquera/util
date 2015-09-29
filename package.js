879525d45031b5fa315ba843e0e13cf9da60e75a
Morten Henriksen <mh@gi2.dk>
2015-09-29 10:10:57 +0200

    version 0.1.17
    Mainly jshint fixes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "ground:util",
-  version: "0.1.16",
+  version: "0.1.17",
   summary: "Adds utillity functions for ground db to use",
   git: "https://github.com/GroundMeteor/util.git"
 });
